7fdbc65c695675d012b623066bd9874943401ae7
TorMagne <tormagne-brekken@outlook.com>
2023-09-07 13:32:03 +0200

    commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'functions.php';
+require 'Database.php';
+//require  'router.php';
+
+$config = require('config.php');
+
+$db = new Database($config );
+
+$posts = $db->query("select * from posts")->fetchAll();
+
+dd($posts);
